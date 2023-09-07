ac510503122e3382cd1b024873e4652ac5245543
oseongmin <202130438@email.daelim.ac.kr>
2023-09-07 16:14:24 +0900

    week02 v0.4

--- week02_numpy.py
+++ week02_numpy.py
@@ -1,9 +1,17 @@
 import numpy as np
+import random
 
 #v = np.array([1, 3, -9, 2])
-v = np.array([
-	[1, 3, -9, 2],
-	[71, 13, -22, 7]])
-print(v.ndim, v.shape, v.data, v.dtype, v.strides)
-print(v)
+#v = np.array([1, 3, -9, 2], dtype='int64')
+#v = np.array([
+#	[1, 3, -9, 2],
+#	[71, 13, -22, 7]])
+#print(v.ndim, v.shape, v.data, v.dtype, v.strides)
+#print(v)
+num = int(input("변수 입력 : "))
+l = list()
+for i in range(num):
+	l.append(random.randint(1, 100))
 
+v = np.array(l, dtype='int16')
+print(v, v.ndim, v.data, v.dtype, v.strides)
