53a2d36e4ac06496bd1c61ce35023d74b18fe8a2
oseongmin <202130438@email.daelim.ac.kr>
2023-09-14 16:26:19 +0900

    week03 v2.0 code

--- week03_numpy.py
+++ week03_numpy.py
@@ -2,11 +2,13 @@ import numpy as np
 import tkinter as tk
 from tkinter import messagebox
 
-def press_enter_key(ev):
-    click_button()
-    #messagebox.showinfo('cordinate value', f"{ev.x}, {ev.y}")
 
-def click_button():
+# def press_enter_key(ev):
+#     click_button()
+#     messagebox.showinfo('x, y', f"({ev.x}, {ev.y})")
+
+
+def click_button(*args):
     try:
         r, c = map(int, en_row_column.get().split())
         matrix = np.random.randint(1, 101, size=(r, c))
@@ -16,7 +18,7 @@ def click_button():
 
 
 window = tk.Tk()
-window.title('numpy gui version v1.7')
+window.title('numpy gui version v2.0')
 window.geometry('300x150')
 
 # create widget
@@ -25,11 +27,14 @@ en_row_column = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # enter key binding with entry widget
+#en_row_column.bind("<Return>", press_enter_key)
 en_row_column.bind("<Return>", click_button)
 
 # widget layout
 lbl_result.pack()
 en_row_column.pack(fill='x')
 btn_click.pack(fill='x')
+
+en_row_column.focus()
 
 window.mainloop()
