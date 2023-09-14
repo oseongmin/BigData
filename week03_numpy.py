2d24292d7e56769f77b43ffb397b9b27d534a1e6
oseongmin <202130438@email.daelim.ac.kr>
2023-09-14 15:32:44 +0900

    week03 v1.3 code copied from voidgogo git

--- week03_numpy.py
+++ week03_numpy.py
@@ -1,52 +1,38 @@
 import numpy as np
 import random
 import tkinter as tk  # Built in GUI
-from tkinter import messagebox #pop-up window
+from tkinter import messagebox  # pop-up window
+
 
 def click_button():
     try:
-        r, c = map(int, en_row_column.get().split())
+        r, c = map(int, en_row_column.get().split())  # spacebar
 
-        rows = list()
-        #rows = ([random.randint(1, 100) for i in range(r)] for i in range(c))
-        for i in range(c):
-            rows.append([random.randint(1, 100) for i in range(r)])
-#        row = [random.randint(1, 100) for i in range(r)]
+        rows = [[random.randint(1, 100) for i in range(r)] for i in range(c)]
         matrix = np.array(rows, dtype='int16')
         lbl_result.config(text=matrix)
     except ValueError as err:
-        messagebox.showerror('Error',f"입력값이 없습니다.\n{err}")
-#        lbl_result.config(text=f"입력값이 없습니다.\n{err}")
+        lbl_result.config(text=f"입력 값이 없습니다.\n{err}")
+        messagebox.showerror('Error!', f"입력 값이 없습니다.\n{err}")
 
 
 window = tk.Tk()
-window.title('numpy gui version v0.7')
+window.title('numpy gui version v1.3')
 window.geometry('300x150')
 
 # create widget
 lbl_result = tk.Label(text="random numpy 2d array")
 en_row_column = tk.Entry()
+#en_column = tk.Entry()
 btn_click = tk.Button(text="click me!", command=click_button)
 
 # widget layout
-#lbl_result.place(50, 50)
-# lbl_result.grid(row=0, column=0, columnspan=2)
-# en_number.grid(row=1, column=0)
-# btn_click.grid(row=1, column=1)
-
-#lbl_result.pack(fill='x')
-
-lbl_result.pack(fill='x')
-en_row_column.pack(fill='x')
-btn_click.pack(fill='x')
-
 # lbl_result.grid(row=0, column=0, columnspan=2)
 # en_row.grid(row=1, column=0)
 # en_column.grid(row=1, column=1)
 # btn_click.grid(row=2, column=0, columnspan=2, sticky=tk.EW)
+lbl_result.pack()
+en_row_column.pack(fill='x')
+btn_click.pack(fill='x')
 
 window.mainloop()
-# n = int(input("input number : "))
-# l = [random.randint(1, 100) for i in range(n)]
-# v = np.array(l, dtype='int16')
-# print(v)
