629fed8b187087edecffeeb110284493899eda32
oseongmin <202130438@email.daelim.ac.kr>
2023-10-05 14:21:34 +0900

    week06 v2.3 moduleClass

---
+++ smrun.py
@@ -0,0 +1,10 @@
+class LinearRegression:
+    def __init__(self):
+        self.slope = None
+        self.intercept = None
+
+    def fit(selfself, X, y):
+        pass
+
+    def predict(self, X):
+        pass
